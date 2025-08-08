84c395ca3e0f8ecffb359c757edb4924ebd5fd9e
tomaseliasfontes <tomaseliasfontes@gmail.com>
2025-08-08 18:34:19 -0300

    Adding the routes for the tickets and events

--- routes/tickets.js
+++ routes/tickets.js
@@ -2,32 +2,81 @@ const express = require("express");
 const router = express.Router();
 
 const ticketsController = require("../controllers/tickets");
-const {handleErrors} = require("../utilities/utilities");
-//const validation = require('../middleware/validate');
-const {isAuthenticated} = require("../middleware/authenticate");
+const { handleErrors } = require("../utilities/utilities");
+const { isAuthenticated } = require("../middleware/authenticate");
 
-// Public routes (no authentication)
-router.get("/", 
-    //#swagger.ignore = true
-    handleErrors(ticketsController.getAll));
-router.get("/:id", 
-    //#swagger.ignore = true
-    handleErrors(ticketsController.getSingle));
+// Public routes
+router.get(
+  "/",
+  /* #swagger.tags = ['Tickets']
+     #swagger.description = 'Get all tickets'
+  */
+  handleErrors(ticketsController.getAll)
+);
 
-// Protected routes (authentication required)
-router.post("/", 
-    //#swagger.ignore = true
-    isAuthenticated, handleErrors(ticketsController.createTicket));
+router.get(
+  "/:id",
+  /* #swagger.tags = ['Tickets']
+     #swagger.description = 'Get a single ticket by ID'
+     #swagger.parameters['id'] = { 
+        in: 'path', 
+        description: 'Ticket ID', 
+        required: true, 
+        type: 'string' 
+     }
+  */
+  handleErrors(ticketsController.getSingle)
+);
+
+// Protected routes
+router.post(
+  "/",
+  /* #swagger.tags = ['Tickets']
+     #swagger.description = 'Create a new ticket'
+     #swagger.parameters['body'] = {
+       in: 'body',
+       description: 'Ticket data',
+       required: true,
+       schema: { $ref: '#/definitions/Ticket' }
+     }
+  */
+  isAuthenticated,
+  handleErrors(ticketsController.createTicket)
+);
+
 router.put(
-  "/:id", 
-    //#swagger.ignore = true
-    isAuthenticated,
+  "/:id",
+  /* #swagger.tags = ['Tickets']
+     #swagger.description = 'Update an existing ticket'
+     #swagger.parameters['id'] = { in: 'path', description: 'Ticket ID', required: true, type: 'string' }
+     #swagger.parameters['body'] = {
+       in: 'body',
+       description: 'Updated ticket data',
+       required: true,
+       schema: { $ref: '#/definitions/Ticket' }
+     }
+  */
+  isAuthenticated,
   handleErrors(ticketsController.updateTicket)
 );
+
+router.put(
+  "/use/:id",
+  /* #swagger.tags = ['Tickets']
+     #swagger.description = 'Mark a ticket as used'
+     #swagger.parameters['id'] = { in: 'path', description: 'Ticket ID', required: true, type: 'string' }
+  */
+  isAuthenticated,
+  handleErrors(ticketsController.markTicketAsUsed)
+);
+
 router.delete(
-  "/:id",  
-    //#swagger.ignore = true
-    isAuthenticated,
+  "/:id",
+  /* #swagger.tags = ['Tickets']
+     #swagger.description = 'Delete a ticket'
+     #swagger.parameters['id'] = { in: 'path', description: 'Ticket ID', required: true, type: 'string' }
+  */
+  isAuthenticated,
   handleErrors(ticketsController.deleteTicket)
 );
 
